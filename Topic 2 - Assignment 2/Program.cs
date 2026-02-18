346f5ef862fccc20131ea11338ffdb321fd589ae
363096132 <363096132@2260I-L29-032>
2026-02-18 12:27:18 -0500

    Add project files.

---
+++ Topic 2 - Assignment 2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Topic_2___Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Task 1: Aayan Shaikh*/
+
+            Console.WriteLine("Task 1: I was born in " + (2026 - 17));
+            Console.WriteLine();
+            Console.WriteLine("Task 2: The average of 14, 15 and 16 is " + ((14 + 15 + 16) / 3));
+            Console.WriteLine();
+            Console.WriteLine("Task 3: The average of 14, 15, 16 and 17 is " + ((14.0 + 15.0 + 16.0 + 17.0) / 4));
+            Console.WriteLine();
+            Console.WriteLine("Task 4: The area of a circle with radius 3 cm is " + (Math.Round(Math.PI * 3 * 3, 1)));
+            Console.WriteLine();
+            Console.WriteLine("Task 5: C# decides whether plus means add or join based on the data type. " +
+                "If both are numbers, it adds them. If one is a string, it joins them as text.");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
